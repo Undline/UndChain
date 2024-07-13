110cb635ff4b30af7e5a362bb7850c88a6f2c44e
Undline <103777919+Undline@users.noreply.github.com>
2024-07-12 20:24:29 -0400

    Update crypto_factory.py
    Added some comments to the methods inside the factory class since that is what will be used in production

--- crypto_factory.py
+++ crypto_factory.py
@@ -27,36 +27,81 @@ class CryptoFactory:
 
     @staticmethod
     def get_crypto_handler() -> CryptoHandler:
+        '''
+        Retrieves the current crypto handler 
+        '''
         if CryptoFactory._crypto_handler is None:
             raise ValueError(f'ERROR: Crypto handler is not set. It must have been set to none elsewhere in the code.')
         return CryptoFactory._crypto_handler
     
     @staticmethod
     def generate_keys() -> Tuple[Any, Any]:
+        '''
+        Generates a key pair suing the current cryptographic handler.
+
+        Returns:
+            tuple[Any, Any]: The generated private and public key pair (in that order)
+        '''
         return CryptoFactory.get_crypto_handler().generate_keys()
     
     @staticmethod
-    def serialize_public_key(public_key: Any) -> str:
+    def view_public_key(public_key: Any) -> str:
+        '''
+        Returns the public key in a readable format
+
+        Returns:
+            str: The public key in PEM format
+        '''
         return CryptoFactory.get_crypto_handler().serialize_public_key(public_key)
     
     @staticmethod
     def save_keys(private_key: Any, public_key: Any, filename: str, directory: str = '.') -> str:
+        '''
+        Saves the key pair locally using the PEM format.
+
+        Returns:
+            str: Returns if file was saved with where it was saved to
+        '''
         return CryptoFactory.get_crypto_handler().save_keys(private_key, public_key, filename, directory)
     
     @staticmethod
     def load_private_key(filepath: str, salt_file_path: str) -> Any:
+        '''
+        Loads the private key from the PEM file
+
+        Returns:
+            Any: Private key
+        '''
         return CryptoFactory.get_crypto_handler().load_private_key(filepath, salt_file_path)
     
     @staticmethod
     def load_public_key(filepath: str) -> Any:
+        '''
+        Loads the public key from PEM file
+
+        Returns:
+            Any: Returns the public key
+        '''
         return CryptoFactory.get_crypto_handler().load_public_key(filepath)
     
     @staticmethod
     def sign_message(private_key: Any, message: bytes) -> bytes:
+        '''
+        Takes in a message and signs it using the private key.
+
+        Returns:
+            bytes: Returns the signature in bytes
+        '''
         return CryptoFactory.get_crypto_handler().sign_message(private_key, message)
     
     @staticmethod
     def verify_signature(public_key: Any, message: bytes, signature: bytes) -> bool:
+        '''
+        Verifies a signature using the public key of the entity who signed it.
+
+        Returns:
+            bool: True if signature is valid, otherwise false.
+        '''
         return CryptoFactory.get_crypto_handler().verify_signature(public_key, message, signature)
     
 
@@ -65,7 +110,7 @@ class CryptoFactory:
 if __name__ == '__main__':
     private_key, public_key = CryptoFactory.generate_keys()
 
-    print(f'Public Key (PEM):\n\n{CryptoFactory.serialize_public_key(public_key)}')
+    print(f'Public Key (PEM):\n\n{CryptoFactory.view_public_key(public_key)}')
 
     print('-' * 44)
 
