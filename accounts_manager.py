5d7dc6b45f3d64a1ea74e05a058be897f7c9836f
Undline <103777919+Undline@users.noreply.github.com>
2024-07-16 21:13:13 -0400

    Update accounts_manager.py
    added a list_accounts and delete_account

--- accounts_manager.py
+++ accounts_manager.py
@@ -24,7 +24,7 @@ class AccountManager:
         '''
         account_path: str = os.path.join(self.accounts_dir, username)
         if os.path.exists(account_path):
-            raise ValueError("Account already exists")
+            raise ValueError(f"ERROR: Account {username} already exists")
         
         os.makedirs(account_path)
 
@@ -58,12 +58,45 @@ class AccountManager:
         '''
         account_path: str = os.path.join(self.accounts_dir, username)
         if not os.path.exists(account_path):
-            raise ValueError('Account does not exist')
+            raise ValueError(f'Account {username} does not exist')
         
         with open(os.path.join(account_path, 'account_info.toml'), 'rb') as file:
             account_info: Dict[str, str] = tomllib.load(file)
 
         return account_info
+    
+    def list_accounts(self) -> Tuple[str, ...]:
+        '''
+        List all accounts saved on the local system.
+
+        Returns:
+            Tuple[str]: List of user account names
+        '''
+        # This folder should only have folders with user names listed
+        return tuple(os.listdir(self.accounts_dir))
+    
+    def delete_account(self, username: str) -> str:
+        '''
+        Deletes the specified account. Other than testing purposes
+        I don't see a reason why a user would delete their account
+        unless they had the keys backed-up elsewhere (YOU WILL LOSE
+        ALL ACCESS TO THE ACCOUNT OTHERWISE)
+
+        Returns:
+            str: Confirmation the account was deleted.
+        '''
+        account_path: str = os.path.join(self.accounts_dir, username)
+        if not os.path.exists(account_path):
+            raise ValueError(f'Account {username} does not exist')
+        
+        for root, dirs, files in os.walk(account_path, topdown=False):
+            for name in files:
+                os.remove(os.path.join(root, name))
+            for name in dirs:
+                os.rmdir(os.path.join(root, name))
+        os.rmdir(account_path)
+
+        return f'Account {username} has been deleted.'
 
 
 # Example use / unit tests
@@ -71,16 +104,29 @@ if __name__ == '__main__':
     manager = AccountManager()
 
     # Create a new account
-    account_path: str = manager.create_account('test_user')
+    account_path: str = manager.create_account('test1')
     print(f'Created an account at: {account_path}')
 
     print('-' * 44)
 
     # Load Account
-    account: Dict[str, str] = manager.load_account('test_user')
-    print(f'Loaded account info for test_user: {account}')
+    account: Dict[str, str] = manager.load_account('test1')
+    print(f'Loaded account info for test_user: \n{account}')
 
     print('-' * 44)
 
+    # List all user accounts
+    accounts: Tuple[str, ...] = manager.list_accounts()
+    print(f'Accounts: {accounts}')
 
-    
+    print('-' * 44)
+
+    # Delete user account
+    delete_result: str = manager.delete_account('test1')
+    print(delete_result)
+
+    print('-' * 44)
+
+    # List accounts to show it was deleted
+    accounts = manager.list_accounts()
+    print(f'Accounts after deletion: {accounts}')
