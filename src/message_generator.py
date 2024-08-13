18a9fb75211c50a47c75268cbc6780fabea32116
Undline <103777919+Undline@users.noreply.github.com>
2024-08-12 21:08:57 -0400

    Update message_generator.py
    added a test case

--- src/message_generator.py
+++ src/message_generator.py
@@ -7,11 +7,11 @@ class MessageGenerator:
     '''
     def __init__(self, message_type: str, message: str) -> None:
         self.version = "2024.08.12" # Need to replace with a method that gets the current version
-        self.timestamp = datetime.utcnow().isoformat()
+        self.timestamp: str = datetime.utcnow().isoformat()
         
         self.message_type: str = message_type
         self.message: str = message
-        self.signature = "Sig"
+        self.signature = self._generate_signature(secret_key='bla')
 
     def _generate_signature(self, secret_key: str) -> str:
         '''
@@ -56,3 +56,14 @@ class MessageGenerator:
             dict: The deserialized message.
         '''
         return eval(data.decode('utf-8'))
+    
+if __name__ == '__main__':
+    msg_gen = MessageGenerator(message_type="JobRequest", message='CoChian:Pages, Type: Storage, Resource:UndChain')
+    generated_message = msg_gen.generate()
+
+    # Test serialized and deserialize
+    serialized_message = MessageGenerator.serialize(generated_message)
+    print(f'Serialized message: {serialized_message}')
+
+    deserialized_message = MessageGenerator.deserialize(serialized_message)
+    print(f'Deserialized message: {deserialized_message}')
