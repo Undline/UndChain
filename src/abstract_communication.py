1ae4e6ada85b4fbb7ffedb07996c0c26d492f820
Undline <103777919+Undline@users.noreply.github.com>
2024-08-16 18:00:22 -0400

    Added get_route
    Added get_route to the abstract class since all protocols will need  a means of contacting a recipient

--- src/abstract_communication.py
+++ src/abstract_communication.py
@@ -14,6 +14,7 @@ class AbstractCommunication(ABC):
         '''
         pass
 
+    @abstractmethod
     def disconnect(self) -> None:
         '''
         Closes the connection with another user
@@ -38,6 +39,38 @@ class AbstractCommunication(ABC):
         pass
 
     @abstractmethod
+    def acknowledge_message(self, message: bytearray) -> bytearray:
+        '''
+        Sends out an acknowledgement that a message was received. Use
+        this when going across lossy / noisy lines. 
+
+        Returns:
+            bytearray: Hash of the message received from the sender.
+        '''
+        pass
+
+    @abstractmethod
+    def get_route(self, recipient: bytearray) -> bytearray:
+        '''
+        Returns a route based off of who the message is being sent to.
+
+        Returns:
+            bytearray: Instruction that the protocol needs in order to 
+            communicate with the targeted device.
+        '''
+        pass
+
+    @abstractmethod
+    def ping(self, recipient: bytearray) -> float:
+        '''
+        Checks the latency and reachability of the recipient
+
+        Returns:
+            float: The latency in milliseconds
+        '''
+        pass
+
+    @abstractmethod
     def translate_address(self, recipient: bytearray) -> bytearray:
         '''
         Translates a username to a network address or physical route 
@@ -56,26 +89,5 @@ class AbstractCommunication(ABC):
 
         Returns:
             str: Route path
-        '''
-        pass
-
-    @abstractmethod
-    def ping(self, recipient: bytearray) -> float:
-        '''
-        Checks the latency and reachability of the recipient
-
-        Returns:
-            float: The latency in milliseconds
-        '''
-        pass
-
-    @abstractmethod
-    def acknowledge_message(self, message: bytearray) -> bytearray:
-        '''
-        Sends out an acknowledgement that a message was received. Use
-        this when going across lossy / noisy lines. 
-
-        Returns:
-            bytearray: Hash of the message received from the sender.
         '''
         pass
