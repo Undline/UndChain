0a2f0a9f9a79b8f569212e1f0c07c6c0623dfe46
Undline <103777919+Undline@users.noreply.github.com>
2024-09-27 19:12:47 -0400

    Create communication_factory.py
    added a communication factory so that we choose the communication type as we read it from the run rules file. Making it dynamic

---
+++ src/communication_factory.py
@@ -0,0 +1,25 @@
+from ip_communication import IPCommunication
+# Add more communication methods as they are made here
+
+class CommunicationFactory:
+    @staticmethod
+    def create_communication(method: str, version: str, co_chain_ID: str):
+        '''
+        This is a factory method used to create the appropriate class
+        based upon the communication method that is passed in. All 
+        communication classes have the same default methods as they 
+        all have to inherit from AbstractCommunication.
+
+        returns:
+            Instance of a communication class
+        '''
+        if method == 'TCP':
+            return IPCommunication(version=version, co_chain_ID=co_chain_ID)
+        # Adding potential communication methods as examples
+        elif method == "LoRA":
+            # Returns a LoRA communication method
+            pass
+        elif method == "Bluetooth":
+            pass
+        else:
+            raise ValueError(f'Unsupported communication method: {method}')
