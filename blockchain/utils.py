3da232aed06f995319528d9d98f3f752219bb04f
VladChernenko <vladchernenko@protonmail.com>
2025-06-13 00:31:13 +0300

    Added stubs for required functions to verify proofs and prepare new epoch. Added some logic to leader rotation thread

--- blockchain/utils.py
+++ blockchain/utils.py
@@ -1,6 +1,6 @@
 import hashlib
 import time
-from structures.threads_metadata_handlers import ApprovementThreadMetadataHandler
+from structures.threads_metadata_handlers import EpochHandler, ApprovementThreadMetadataHandler
 from globals import CORE_MAJOR_VERSION
 
 def sha256(data: str) -> str:
@@ -14,3 +14,22 @@ def is_my_core_version_old(thread: ApprovementThreadMetadataHandler) -> bool:
 
 def epoch_still_fresh(thread: ApprovementThreadMetadataHandler) -> bool:
     thread.epoch.start_timestamp+thread.network_parameters.get("EPOCH_TIME") > get_utc_timestamp()
+
+
+def get_from_approvement_thread_state(validator_id: str):
+    pass
+
+
+def set_leaders_sequence(thread: ApprovementThreadMetadataHandler, epoch_seed: str):
+    pass
+
+def get_quorum_majority(thread: ApprovementThreadMetadataHandler) -> int:
+    pass
+
+
+def get_quorum_urls_and_pubkeys(thread: ApprovementThreadMetadataHandler):
+    pass
+
+
+def get_current_epoch_quorum(epoch_handler: EpochHandler) -> List[str]:
+    pass
