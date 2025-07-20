45e413456e359e6f7dc9bcb464fc38f06e3a3057
Undline <103777919+Undline@users.noreply.github.com>
2025-07-20 13:21:02 -0400

    Create base_packet_generator.py
    Created to keep track of basic messages on the network

---
+++ src/base_packet_generator.py
@@ -0,0 +1,6 @@
+'''
+The purpose of this class is because every user type will have a common
+set of packets that they will send across teh network. This is meant to 
+keep them all in one module to make it easier to debug and manage
+'''
+
