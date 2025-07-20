20b88d1c13e335f7ff4e1dfa614c2026a75f6107
Undline <103777919+Undline@users.noreply.github.com>
2025-07-20 16:56:46 -0400

    Create base_packet_handler.py

---
+++ src/base_packet_handler.py
@@ -0,0 +1,7 @@
+'''
+This class was made to complienmenmt the base packet generator as these are 
+all the responces for every packet sent via the handler. 
+
+Remember: The packet handler manages network events where the generator handles
+user initiated requests.
+'''
